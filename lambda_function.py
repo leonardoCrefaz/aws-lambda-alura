896343054b66a6e9ee6f3965514d7b9edc8506e6
Leonardo Vieira <leonardo.ramos@crefaz.com.br>
2023-11-03 10:58:03 -0300

    Buscando objetos no S3

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,24 @@
 import json
 from log import log
+import boto3
+
+s3 = boto3.resource("s3")
 
 def lambda_handler(event, context):
 
     log(f"Event: {json.dumps(event)}")
     log(f"Context: {context.invoked_function_arn}")
 
-    return {
-        "statusCode": 200,
-        "body": f"<html><body>Dados da requisicao {json.dumps(event)}</body></html>",
-        "headers": {
-            "content-type": "text/html",
-        }
-    }
+    Bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    Key = event["Records"][0]["s3"]["object"]["key"]
+
+    getObjectResult = s3.Object(bucket_name=Bucket, key=Key)
+
+    mega_byte = 1024 * 1024
+
+    if (getObjectResult.content_length > 1 * mega_byte):
+        log("Objeto muito grande")
+        return "Objeto muito grande"
+
+    print("Objeto de tamanho OK")
+    return "Objeto de tamanho OK"
